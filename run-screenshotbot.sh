e1f372b03c47780afb76c8a0e8349f328fc0304a
Arnold Noronha <arnold@tdrhq.com>
2022-02-10 12:02:49 -0500

    ..

--- run-screenshotbot.sh
+++ run-screenshotbot.sh
@@ -1,5 +1,5 @@
 
 curl https://screenshotbot.io/recorder.sh | sh
-~/screenshotbot/recoder --record-static-website \
- --api-key "$SCREENSHOTBOT_API_KEY"
+~/screenshotbot/recorder --record-static-website \
+ --api-key "$SCREENSHOTBOT_API_KEY" \
  --api-secret "$SCREENSHOTBOT_API_SECRET"
