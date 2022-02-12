a70a4699b0fe541565db3e99abae1fa420072f3a
Arnold Noronha <arnold@tdrhq.com>
2022-02-12 15:54:22 -0500

    Attempt full build

--- run-screenshotbot.sh
+++ run-screenshotbot.sh
@@ -1,6 +1,3 @@
 
 curl https://screenshotbot.io/recorder.sh | sh
-~/screenshotbot/recorder --static-website public/ \
- --channel gatsby-example \
- --api-key "$SCREENSHOTBOT_API_KEY" \
- --api-secret "$SCREENSHOTBOT_API_SECRET" \
+~/screenshotbot/recorder --static-website public/
