0a963953624669f05d70b2187a236d48cbee39c0
Arnold Noronha <arnold@tdrhq.com>
2022-02-12 17:40:25 -0500

    Do I really need this?

--- run-screenshotbot.sh
+++ run-screenshotbot.sh
@@ -1,4 +1,4 @@
-git remote add origin $REPOSITORY_URL
-git fetch origin master
+# git remote add origin $REPOSITORY_URL
+# git fetch origin master
 curl https://screenshotbot.io/recorder.sh | sh
 ~/screenshotbot/recorder --static-website public/ --main-branch master
