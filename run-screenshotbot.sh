99a101d1a4ca52930552ad89e60216c077a72044
Arnold Noronha <arnold@tdrhq.com>
2022-02-12 17:36:32 -0500

    ..

--- run-screenshotbot.sh
+++ run-screenshotbot.sh
@@ -1,4 +1,4 @@
-git remote
+git remote add origin $REPOSITORY_URL
 git fetch origin main
 curl https://screenshotbot.io/recorder.sh | sh
 ~/screenshotbot/recorder --static-website public/ --main-branch main
