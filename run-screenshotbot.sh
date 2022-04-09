fd40621ed6983d7ea3970f021141cc1b16a11f8c
Arnold Noronha <arnold@tdrhq.com>
2022-04-08 22:33:56 -0400

    fix script

--- run-screenshotbot.sh
+++ run-screenshotbot.sh
@@ -1,3 +1,5 @@
+git remote add origin https://github.com/tdrhq/gatsby-example
 git fetch origin master
+
 curl https://screenshotbot.io/recorder.sh | sh
 ~/screenshotbot/recorder --static-website public/ --main-branch master
